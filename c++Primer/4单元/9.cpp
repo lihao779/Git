d86c65e53b6d152055ddc77c8e8fea3f8a2affda
lihao779 <3024978915@qq.com>
2020-12-17 23:02:44 +0800

    c++

--- c++Primer/4单元/9.cpp
+++ c++Primer/4单元/9.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+
+/*
 int fact(int val)
 {
     int ret = 1;
@@ -10,10 +12,28 @@ int fact(int val)
     }
     return ret;
 }
+*/
+
+int fact(int val)
+{
+    int ret = 1;
+    while(val > 1)
+    {
+        ret *= val--;
+    }
+    return ret;
+}
+int staticfun()
+{
+    static int a = 0;
+    return a++;
+}
+
 
 
 int main()
 {
-    cout << "3! is"<<fact(3)<<endl;
+    for(int i = 0;i<10;i++)
+        cout<<staticfun()<<endl;
     return 0;
 }
