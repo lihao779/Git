b82cebc27314942e93d0029a6f78255f065f652a
lihao779 <3024978915@qq.com>
2021-01-08 18:35:55 +0800

    RBTree

--- 2021/202101/RBTree/RBTree.hpp
+++ 2021/202101/RBTree/RBTree.hpp
@@ -39,6 +39,7 @@ class RBTree
             root = new Node(date,BLACK);
             head->left = root;
             head->right = root;
+            head->parent = root;
             root->parent = head;
             return true;
         }
@@ -60,20 +61,111 @@ class RBTree
         else
             parent->right = cur;
         cur->parent = parent;
+        // 新节点插入后 由于新节点默认颜色是红色-
+        // 如果parent节点的颜色是黑色--红黑树的性质没有遭到破坏
+        // 如果parent节点的颜色是红色的--红黑树性质三-不能有两个连在一起的红色节点遭到破坏--需要调整
+        
+        while(parent->color == RED && parent != head)
+        {
+            Node* grandfater = parent->parent;
+            if(parent == grandfater->left)
+            {
+                Node* uncle = grandfater->right;
+                if(uncle && uncle->color == RED)
+                {
+                    uncle->color = BLACK;
+                    parent->color = BLACK;
+                    grandfater->color = RED;
+                    
+                    //继续往上更新
+                    cur = grandfater;
+                    parent = cur->parent;
+                }
+                else
+                {
+                    if(cur == parent->right)
+                    {
+                        RotateLeft(parent);/////////////
+                        swap(cur,parent);/////////////
+                    }
+                    parent->color = BLACK;
+                    grandfater->color = RED;
+                    RotateRight(grandfater);
+                }
+                
+            }
+            else
+            {
+                Node* uncle = parent->left;
+                if(uncle && uncle->color == RED)
+                {
+                    uncle->color = BLACK;
+                    parent->color = BLACK;
+                    grandfater->color = RED;
+                    
+                    //继续往上更新
+                    cur = grandfater;
+                    parent = cur->parent;
+                }
+                else
+                {
+                    if(cur == parent->left)
+                    {
+                        RotateRight(parent);
+                        swap(parent,cur);
+                    }
+                    parent->color = BLACK;
+                    grandfater->color = RED;
+                    RotateLeft(grandfater);
+                }
+
+            }
+
+        }
+        
 
 
-
-
-
-        //....
-        //
-        //
+        root->color = BLACK;
         head->left = LeftMost();
         head->right = RightMost();
         return true;
     }
     
     private:
+    void RotateLeft(Node* parent)
+    {
+        Node* grandfater = parent->parent;
+        Node* subR = parent->right;
+        Node* subRL = subR->left;
+        
+        parent->right = subRL;
+        if(subRL)
+            subRL->parent = parent;
+        subR->left = parent;
+        parent->parent = subR;
+        subR->parent = grandfater;
+
+    }
+    void RotateRight(Node* parent)
+    {
+        Node* grandfater = parent->parent;
+        Node* subL = parent->left;
+        Node* subLR = subL->right;
+
+        parent->left = subLR;
+        if(subLR)
+            subLR->parent = parent;
+        subL->right = parent;
+        parent->parent = subL;
+        subL->parent = grandfater;
+    }
+
+    void swap(Node*& left,Node*& right)
+    {
+        Node* tmp = left;
+        left = right;
+        right = tmp;
+    }
     Node* LeftMost()
     {
         Node* cur = GetRoot();
